64ee2c65f66d7c453e0c21f79075258bced5c570
willder-root <willderaraujo_99_gds@hotmail.com>
2025-03-05 11:08:08 -0300

    Novas alterações 4

--- src/main/java/br/com/javateste/testeinicojava/maven/Hello.java
+++ src/main/java/br/com/javateste/testeinicojava/maven/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World whith changes 2!");
 		System.out.println("Hello World whith changes 3!");
+		System.out.println("Hello World whith changes 4!");
 	}
 
 }
